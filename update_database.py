82b9b0b96e2fa28019d282e0462a5b3bfadbee41
witvliet <daniel.witvliet@gmail.com>
2021-02-14 00:23:34 -0500

    Load all data into dataframe

--- update_database.py
+++ update_database.py
@@ -1,23 +1,42 @@
 import os
 import zipfile
 import urllib.request
+
+import pandas as pd
 from google.cloud import bigquery
 
 if not os.path.exists('data'):
     os.mkdir('data')
 
 data_path = 'data/COVID-19-master/csse_covid_19_data/csse_covid_19_daily_reports'
-vaccinations_path = 'data/vaccinations.csv'
 
-
-#urllib.request.urlretrieve('https://github.com/owid/covid-19-data/raw/master/public/data/vaccinations/vaccinations.csv', vaccinations_path)
 #urllib.request.urlretrieve('https://github.com/CSSEGISandData/COVID-19/archive/master.zip', 'data/covid.zip')
 # with zipfile.ZipFile('data/covid.zip', 'r') as zip_ref:
 #     zip_ref.extractall('data')
 
-client = bigquery.Client()
+# Load data into DataFrame.
+csv_files = [f for f in os.listdir(data_path) if f.endswith('.csv')]
+dfs = []
+for csv_file in csv_files:
+    month, day, year = csv_file[:-4].split('-')
+
+    df = pd.read_csv(os.path.join(data_path, csv_file)).rename({
+        'Country_Region': 'country', 'Country/Region': 'country',
+        'Confirmed': 'cases', 'Deaths': 'deaths', 'Recovered': 'recovered',
+    }, axis=1)
+
+    if len({'country', 'cases', 'deaths', 'recovered'}.intersection(df.columns)) != 4:
+        print('Some columns are missing!')
+        continue
+
+    df = df.groupby('country')[['cases', 'deaths', 'recovered']].sum()
+    df['date'] = f'{year}-{month}-{day}'
+    dfs.append(df)
+
+df = pd.concat(dfs)
 
 # Create BigQuery table if it does not exists.
+client = bigquery.Client()
 table_id = "covid-19-dashboard-304803.covid19_data.cases_and_vaccinations"
 schema = [
     bigquery.SchemaField('country', 'STRING', mode='REQUIRED'),
@@ -25,9 +44,9 @@ schema = [
     bigquery.SchemaField('cases', 'INTEGER'),
     bigquery.SchemaField('deaths', 'INTEGER'),
     bigquery.SchemaField('recovered', 'INTEGER'),
-    bigquery.SchemaField('vaccination_doses', 'INTEGER'),
-    bigquery.SchemaField('vaccination_people', 'INTEGER'),
-    bigquery.SchemaField('vaccination_people_fully', 'INTEGER'),
+#     bigquery.SchemaField('vaccination_doses', 'INTEGER'),
+#     bigquery.SchemaField('vaccination_people', 'INTEGER'),
+#     bigquery.SchemaField('vaccination_people_fully', 'INTEGER'),
 ]
 table = bigquery.Table(table_id, schema=schema)
 table = client.create_table(table, exists_ok=True)
