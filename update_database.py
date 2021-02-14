e4dfee19b5626e5f0283964dc0592e40f7c6f72f
witvliet <daniel.witvliet@gmail.com>
2021-02-13 23:18:24 -0500

    Added template code for creating BigQuery table

--- update_database.py
+++ update_database.py
@@ -1,6 +1,7 @@
 import os
 import zipfile
 import urllib.request
+from google.cloud import bigquery
 
 if not os.path.exists('data'):
     os.mkdir('data')
@@ -9,7 +10,19 @@ data_path = 'data/COVID-19-master/csse_covid_19_data/csse_covid_19_daily_reports
 vaccinations_path = 'data/vaccinations.csv'
 
 
-urllib.request.urlretrieve('https://github.com/owid/covid-19-data/raw/master/public/data/vaccinations/vaccinations.csv', vaccinations_path)
-urllib.request.urlretrieve('https://github.com/CSSEGISandData/COVID-19/archive/master.zip', 'data/covid.zip')
-with zipfile.ZipFile('data/covid.zip', 'r') as zip_ref:
-    zip_ref.extractall('data')
+#urllib.request.urlretrieve('https://github.com/owid/covid-19-data/raw/master/public/data/vaccinations/vaccinations.csv', vaccinations_path)
+#urllib.request.urlretrieve('https://github.com/CSSEGISandData/COVID-19/archive/master.zip', 'data/covid.zip')
+# with zipfile.ZipFile('data/covid.zip', 'r') as zip_ref:
+#     zip_ref.extractall('data')
+
+client = bigquery.Client()
+
+table_id = "covid-19-dashboard-304803.covid19_data.your_table_name"
+
+schema = [
+    bigquery.SchemaField('full_name', 'STRING', mode='REQUIRED'),
+    bigquery.SchemaField('age', 'INTEGER', mode='REQUIRED'),
+]
+
+table = bigquery.Table(table_id, schema=schema)
+table = client.create_table(table, exists_ok=True)
