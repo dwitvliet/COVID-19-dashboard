bb217f9deee4b9909a5928e406b6d1d2536d4001
witvliet <daniel.witvliet@gmail.com>
2021-02-15 03:33:17 -0500

    Tweaked datastudio fields

--- datastudio/data_fields.sql
+++ datastudio/data_fields.sql
@@ -1,21 +1,21 @@
 CASE
-    WHEN metric = 'cases_total' THEN 'Total cases'
-    WHEN metric = 'cases_per_person' THEN 'Total cases per person'
-    WHEN metric = 'cases_new' THEN 'New cases (weekly moving average)'
-    WHEN metric = 'new_cases_per_person' THEN 'New cases per person'
+    WHEN metric = 'cases_total' THEN 'Cases, total'
+    WHEN metric = 'cases_per_person' THEN 'Cases, total per person'
+    WHEN metric = 'cases_new' THEN 'Cases, new (weekly moving average)'
+    WHEN metric = 'new_cases_per_person' THEN 'Cases, new per person'
 
-    WHEN metric = 'deaths_total' THEN 'Total deaths'
-    WHEN metric = 'deaths_per_person' THEN 'Total deaths per person'
-    WHEN metric = 'deaths_new' THEN 'New deaths (weekly moving average)'
-    WHEN metric = 'new_deaths_per_person' THEN 'New deaths per person'
+    WHEN metric = 'deaths_total' THEN 'Deaths, total'
+    WHEN metric = 'deaths_per_person' THEN 'Deaths, total per person'
+    WHEN metric = 'deaths_new' THEN 'Deaths, new (weekly moving average)'
+    WHEN metric = 'new_deaths_per_person' THEN 'Deaths, new per person'
 
-    WHEN metric = 'tests_total' THEN 'Total tests'
-    WHEN metric = 'tests_per_person' THEN 'Total tests per person'
-    WHEN metric = 'tests_new' THEN 'New tests (weekly moving average)'
-    WHEN metric = 'new_tests_per_person' THEN 'New tests per person'
+    WHEN metric = 'tests_total' THEN 'Tests, total'
+    WHEN metric = 'tests_per_person' THEN 'Tests, total per person'
+    WHEN metric = 'tests_new' THEN 'Tests, new (weekly moving average)'
+    WHEN metric = 'new_tests_per_person' THEN 'Tests, new per person'
 
     WHEN metric = 'deaths_per_case' THEN 'Fatality rate'
-    WHEN metric = 'cases_per_test' THEN 'Proportion positive tests'
+    WHEN metric = 'cases_per_test' THEN 'Tests, proportion positive'
 
     ELSE 'Other'
 END
