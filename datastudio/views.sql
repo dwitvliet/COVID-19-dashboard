a42bd10d68c36e581604be17718ad5dfc247cf06
witvliet <daniel.witvliet@gmail.com>
2021-02-16 00:37:39 +0700

    Updated view with positive tests

--- datastudio/views.sql
+++ datastudio/views.sql
@@ -1,7 +1,16 @@
-CREATE VIEW `covid19_data.fatality_rate` AS
+CREATE VIEW covid19_data.cases_global AS
 SELECT
     `date`,
-    SUM(deaths_total) / SUM(cases_total) AS fatality_rate
+    SUM(cases_total) AS cases_total,
+    SUM(cases_new) AS cases_new,
+    CASE
+        WHEN SUM(tests_total) = 0 THEN NULL
+        ELSE SUM(deaths_total) / SUM(cases_total)
+    END AS fatality_rate,
+    CASE
+        WHEN SUM(tests_total) = 0 THEN NULL
+        ELSE SUM(cases_total) / SUM(tests_total)
+    END AS cases_per_test_pct,
 FROM `covid19_data.cases`
 GROUP BY `date`
 ORDER BY `date` DESC;
